06cab9d90eb48e0a4e3d6f7b14bbca32bf7d1d88
EvaIT135 <emalshi2@uet.edu.al>
2026-06-06 11:09:59 +0000

    Ruaj

--- backend/controllers/bookingController.js
+++ backend/controllers/bookingController.js
@@ -5,13 +5,13 @@ export const krijoRezervim = async (req, res) => {
     const booking = await Booking.create({
       user: req.user._id,
       event: req.body.eventId,
-      nrBiletash: req.body.nrBiletash
+      nrBiletash: req.body.nrBiletash,
     });
 
     res.status(201).json(booking);
   } catch (error) {
     res.status(500).json({
-      message: error.message
+      message: error.message,
     });
   }
 };
@@ -19,15 +19,17 @@ export const krijoRezervim = async (req, res) => {
 export const rezervimetEMia = async (req, res) => {
   try {
     const bookings = await Booking.find({
-      user: req.user._id
+      user: req.user._id,
     }).populate("event");
 
     res.status(200).json(bookings);
   } catch (error) {
     res.status(500).json({
-      message: error.message
+      message: error.message,
     });
   }
+};
+
 export const anuloRezervim = async (req, res) => {
   try {
     const booking = await Booking.findById(req.params.id);
@@ -54,6 +56,4 @@ export const anuloRezervim = async (req, res) => {
       message: error.message,
     });
   }
-};
-
 };
